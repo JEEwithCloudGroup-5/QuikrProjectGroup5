427fe05a3842be3eb23a871a17f69229733b3548
128996 <mallika.kamma@gmail.com>
2019-01-19 17:50:00 +0530

    committed angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { LoginComponent } from './login/login.component';
+import { RegComponent } from './reg/reg.component';
+import { ReactiveFormsModule }    from '@angular/forms';
+import { ShowHidePasswordModule } from 'ngx-show-hide-password';
+import { AngularEntypoModule } from 'angular-entypo';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    routingComponents,
+    ProductComponent,
+    LoginComponent,
+   RegComponent,
+   HeaderComponent,
+   FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,FormsModule,ReactiveFormsModule, HttpClientModule,
+     ShowHidePasswordModule.forRoot(),AngularEntypoModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
